21fe04607458b2f4e89407b01ddfb50aafda456c
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2020-04-12 22:22:31 +0530

    Random Forest added

---
+++ Random Forest/decision_tree.py
@@ -0,0 +1,112 @@
+import numpy as np
+from collections import Counter
+
+
+def giniImpurity(y):
+    hist = np.bincount(y)
+    ps = hist / len(y)
+    return np.sum(ps * (1 - ps))
+
+
+class Node:
+
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+
+class DecisionTree():
+
+    def __init__(self, min_samples_split=5, max_depth=100, n_feats=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_feats = n_feats  # for random forest, choosing a subset of features
+        self.root = None
+
+    def fit(self, X, y):
+        self.n_feats = X.shape[1] if not self.n_feats else min(
+            self.n_feats, X.shape[1])
+        self.root = self._grow_tree(X, y)
+
+    def _grow_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        # stopping criteria
+        if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_features, self.n_feats, replace=False)
+
+        # greedily select the best split according to the gini gain
+        best_feat, best_thresh = self._best_criteria(X, y, feat_idxs)
+
+        # grow the children that result from the split
+        left_idxs, right_idxs = self._split(X[:, best_feat], best_thresh)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth+1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth+1)
+        return Node(best_feat, best_thresh, left, right)
+
+    def _best_criteria(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_thresh = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._gini_gain(y, X_column, threshold)
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_thresh = threshold
+
+        return split_idx, split_thresh
+
+    def _gini_gain(self, y, X_column, split_thresh):
+        # parent
+        parent_gini_impurity = giniImpurity(y)
+
+        # generate split
+        left_idxs, right_idxs = self._split(X_column, split_thresh)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+
+        # compute the weighted avg. for the children
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        g_l, g_r = giniImpurity(y[left_idxs]), giniImpurity(y[right_idxs])
+        child_gini_impurity = (n_l / n) * g_l + (n_r / n) * g_r
+
+        # gini gain is difference in loss before vs. after split
+        gg = parent_gini_impurity - child_gini_impurity
+        return gg
+
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
+        return left_idxs, right_idxs
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
